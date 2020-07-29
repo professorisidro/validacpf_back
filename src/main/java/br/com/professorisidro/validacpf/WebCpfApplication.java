7f19e36a5f685c35ad605bd4247e7668e4c6d6b7
professorisidro <isidro@professorisidro.com.br>
2020-07-29 11:33:03 -0300

    First Commit

---
+++ src/main/java/br/com/professorisidro/validacpf/WebCpfApplication.java
@@ -0,0 +1,13 @@
+package br.com.professorisidro.validacpf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCpfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebCpfApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/professorisidro/validacpf/controller/ValidadorController.java
@@ -0,0 +1,22 @@
+package br.com.professorisidro.validacpf.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.professorisidro.validacpf.service.Validador;
+
+@RestController
+@CrossOrigin("*")
+public class ValidadorController {
+	
+	@GetMapping("/validacpf")
+	public String validaCPF(@RequestParam String cpf) {
+		if (Validador.isCPF(cpf)) {
+			return "valido";
+		}
+		return "invalido";
+	}
+
+}
